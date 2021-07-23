34019f2ebb22c56d81fd9da3d6005788974fbc28
Anushka Paul <87390353+pilipi-puu-puu@users.noreply.github.com>
2021-07-23 21:57:56 +0530

    Added files

---
+++ Easy/power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{   int base,power;
+    printf("Enter the base:");
+    scanf("%d",&base);
+    printf("\nEnter the power");
+    scanf("%d",&power);
+    printf("\n%f",pow(base,power));
+
+}
